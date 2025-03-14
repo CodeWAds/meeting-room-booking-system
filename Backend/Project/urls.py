776710deb5a44f1ed5d46b2bdb3caa232da6ce8c
deumir404 <deumir@yandex.ru>
2025-03-14 11:47:52 +0300

    Hello world

--- Backend/Project/urls.py
+++ Backend/Project/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import include, path
 """
 URL configuration for project project.
 
@@ -14,9 +16,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
+    path("hello/", include("apps.hello.urls")),
     path('admin/', admin.site.urls),
 ]
