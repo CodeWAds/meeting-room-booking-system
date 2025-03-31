6412d2a8e13d9faa4446515a832c2bbb94abaac8
deumir404 <deumir@yandex.ru>
2025-03-31 22:15:44 +0300

    Add endpoint for auth

--- Backend/apps/user/urls.py
+++ Backend/apps/user/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("login_telegram", views.login_by_telegram, name="login_by_telegram"),
+    path("login_password", views.user_login, name="user_login"),
+    path("logout", views.user_logout, name="user_logout"),
     path("", views.get_users, name="get_users"),  
     path("create-user/", views.user_create, name="create-user"),
     path("<int:user_id>/", views.get_user, name="user-detail"), 
